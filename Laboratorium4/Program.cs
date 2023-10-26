2cbce16d02cdd17fb73fb65f0f1c865eaa2b5dfe
Usadowiony <wojciechpietrzak2003@gmail.com>
2023-10-26 12:06:56 +0200

    make zadanie 1

--- Laboratorium4/Program.cs
+++ Laboratorium4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void Zadanie1(){
+    double[] factors = { 2.4, 1.9, -0.5};
+    double x = 2.3;
+    if(factors.Length < 3){
+        Console.WriteLine("Nie można obliczyć równania, nie ma wszystkich współczynników.");
+    }else{
+        Console.WriteLine($"Wynik równania kwadratowego wynosi: {(factors[0]*x*x) + (factors[1]*x) + factors[2]}");
+    }
+}
+Zadanie1();
