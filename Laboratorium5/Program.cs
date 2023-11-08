ecf00e7d7ec8ae7bb82371686a7440871a632f25
Wojciech Pietrzak <118462265+Usadowiony@users.noreply.github.com>
2023-11-08 09:28:43 +0100

    add laboratorium5 project

---
+++ Laboratorium5/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Laboratorium5{
+    internal class Program{
+        static void Main(string[] arg)
+        {
+            Print(5.5);
+        }
+        static void Print(double x)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
