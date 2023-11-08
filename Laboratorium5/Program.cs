b017fbd794cebf9220eac08fecb9fb71f2dbba25
Wojciech Pietrzak <118462265+Usadowiony@users.noreply.github.com>
2023-11-08 09:35:46 +0100

    create example procedure

--- Laboratorium5/Program.cs
+++ Laboratorium5/Program.cs
@@ -2,11 +2,16 @@
     internal class Program{
         static void Main(string[] arg)
         {
-            Print(5.5);
+            Example();
         }
         static void Print(double x)
         {
             Console.WriteLine(x);
+        }
+        static void Example(){
+            Print(5.5);
+            double x = 2.5;
+            Print(x * 2);
         }
     }
 }
