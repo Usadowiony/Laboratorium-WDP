6f59dce332c2627bde79642252519bf71d2622e6
Wojciech Pietrzak <wojciechpietrzak2003@gmail.com>
2023-11-11 10:42:13 +0100

    make zadanie 2

--- Laboratorium5/Program.cs
+++ Laboratorium5/Program.cs
@@ -2,11 +2,19 @@
     internal class Program{
         static void Main(string[] arg)
         {
-            Print(3.1);
+            Console.WriteLine(CircleArea(2.2));
         }
         //Zadnie 1
         public static void Print(double a){
             Console.WriteLine($"Wartość argumentu jest równa = {a}");
+        }
+        //Zadanie 2
+        public static double CircleArea(double r){
+            if(r <= 0){
+                return double.NaN;
+            }
+            double area = Math.PI * (r * r);
+            return area;
         }
     }
 }
