3a6f33bb63eb6706f9ee045f64e58208344ee6eb
Usadowiony <wojciechpietrzak2003@gmail.com>
2023-11-13 19:58:20 +0100

    add terminal error color

--- Laboratorium5/Program.cs
+++ Laboratorium5/Program.cs
@@ -25,7 +25,30 @@
             double x;
             Console.Write("Podaj w formacie double liczbe x: ");
             while(double.TryParse(Console.ReadLine(), out x)){
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write("Niepoprawny format! Wpisz jeszcze raz: ");
+                Console.ResetColor();
+            }
+            double a;
+            Console.Write("Podaj w formacie double liczbe a: ");
+            while(double.TryParse(Console.ReadLine(), out a)){
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Niepoprawny format! Wpisz jeszcze raz: ");
+                Console.ResetColor();
+            }
+            double b;
+            Console.Write("Podaj w formacie double liczbe b: ");
+            while(double.TryParse(Console.ReadLine(), out b)){
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Niepoprawny format! Wpisz jeszcze raz: ");
+                Console.ResetColor();
+            }
+            double c;
+            Console.Write("Podaj w formacie double liczbe c: ");
+            while(double.TryParse(Console.ReadLine(), out c)){
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Niepoprawny format! Wpisz jeszcze raz: ");
+                Console.ResetColor();
             }
         }
     }
