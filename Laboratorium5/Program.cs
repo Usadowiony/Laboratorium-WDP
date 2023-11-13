cf43bc3de790cf70ec4e5965d5d47ce1c3594789
Usadowiony <wojciechpietrzak2003@gmail.com>
2023-11-13 20:34:15 +0100

    optimization code in Zadanie4

--- Laboratorium5/Program.cs
+++ Laboratorium5/Program.cs
@@ -22,33 +22,32 @@
         }
         ///Zadanie 4
         public static void Zadanie4(){
-            double x;
-            Console.Write("Podaj w formacie double liczbe x: ");
-            while(double.TryParse(Console.ReadLine(), out x)){
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("Niepoprawny format! Wpisz jeszcze raz: ");
-                Console.ResetColor();
-            }
-            double a;
-            Console.Write("Podaj w formacie double liczbe a: ");
-            while(double.TryParse(Console.ReadLine(), out a)){
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("Niepoprawny format! Wpisz jeszcze raz: ");
-                Console.ResetColor();
-            }
-            double b;
-            Console.Write("Podaj w formacie double liczbe b: ");
-            while(double.TryParse(Console.ReadLine(), out b)){
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("Niepoprawny format! Wpisz jeszcze raz: ");
-                Console.ResetColor();
-            }
-            double c;
-            Console.Write("Podaj w formacie double liczbe c: ");
-            while(double.TryParse(Console.ReadLine(), out c)){
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("Niepoprawny format! Wpisz jeszcze raz: ");
-                Console.ResetColor();
+            double[] values = new double[4];
+            for(int i = 0; i < 4; i++){
+                string letter = "";
+                double value = 0.0;
+                    switch (i){
+                        case 0:
+                            letter = "x";
+                            break;
+                        case 1:
+                            letter = "a";
+                            break;
+                        case 2:
+                            letter = "b";
+                            break;
+                        case 3:
+                            letter = "c";
+                            break;
+                        }
+                    Console.Write($"Podaj w formacie double liczbe {letter}: ");
+                    while(double.TryParse(Console.ReadLine(), out value)){
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("Niepoprawny format! Wpisz jeszcze raz: ");
+                        Console.ResetColor();
+                    }
+                    values[i] = value;
+                    Console.WriteLine($"Wartość dla {letter}: {values[i]}");
             }
         }
     }
