a01345c0c6cafcc6aee60c3cf835d3cbde34de01
Wojciech Pietrzak <118462265+Usadowiony@users.noreply.github.com>
2023-11-22 09:24:41 +0100

    add laboratorium6 project

---
+++ Laboratorium6/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Laboratorium6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Car {
+        public int Power;
+        public int Volume;
+    }
+}
