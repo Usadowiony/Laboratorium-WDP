00fb37d88ed2f37148df5a4b58ac839d807b8944
Wojciech Pietrzak <118462265+Usadowiony@users.noreply.github.com>
2023-11-22 09:27:03 +0100

    add car and car2 class

--- Laboratorium6/Program.cs
+++ Laboratorium6/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car = new Car();
+            Car car2 = new Car();
+            car.Power = 100;
+            car2.Power = 200;
+            Console.WriteLine(car.Power);
+            Console.WriteLine(car2.Power);
+
         }
     }
 
