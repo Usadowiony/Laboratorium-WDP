11ec3fc7cfbc8a71d24556efc19468791f52480d
Wojciech Pietrzak <118462265+Usadowiony@users.noreply.github.com>
2023-11-22 09:36:11 +0100

    add car3

--- Laboratorium6/Program.cs
+++ Laboratorium6/Program.cs
@@ -10,12 +10,22 @@
             car2.Power = 200;
             Console.WriteLine(car.Power);
             Console.WriteLine(car2.Power);
-
+            Car car3 = new Car(200, 2000);
         }
     }
 
     class Car {
         public int Power;
         public int Volume;
+        private double _mileage;
+        public Car(int power, int volume){
+            Power = power;
+            Volume = volume;
+            _mileage = 0;
+        }
+        public Car(){
+            Power = 50;
+            Volume = 500;
+        }
     }
 }
