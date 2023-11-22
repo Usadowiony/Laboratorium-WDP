66e5f172e6f024f0737758b8c8cd65f950e7d74c
Wojciech Pietrzak <118462265+Usadowiony@users.noreply.github.com>
2023-11-22 10:29:02 +0100

    commit

--- Laboratorium6/Program.cs
+++ Laboratorium6/Program.cs
@@ -4,34 +4,81 @@
     {
         static void Main(string[] args)
         {
-            Car car3 = new Car(200, 2000, 60, 7.5);
+            Car car3 = new Car{
+                Power = 200,
+                Volume = 2000,
+                FuelConsumption = 7.5,
+                FuelTankCapacity = 60,
+            };
             car3.Drive(15);
             car3.Drive(25);
-            Console.WriteLine(car3.GetMileage());
             car3.Refuel();
             car3.Drive(120);
             car3.Drive(200);
-            Console.WriteLine($"Car Mileage: { car3.GetMileage()}");
-            Console.WriteLine($"Car FuelLevel: {car3.GetFuelLevel()}");
+            Console.WriteLine($"Car Mileage: { car3.GetMileage}");
+            Console.WriteLine($"Car FuelLevel: {car3.GetFuelLevel}");
+            Console.WriteLine($"Car CanDrive: {car3.Distance}");
         }
     }
 
     class Car {
-        public int Power;
-        public int Volume;
+        private int _power;
+        public int Power{
+            get{
+                return _power;
+            }
+            set{
+                if (_power > 0){
+                    _power = value;
+                }
+            }
+        }
+
+        private int _volume;
+        public int Volume{
+            get{
+                return _volume;
+            }
+            set{
+                if( _volume > 0){
+                    _volume = value;
+                }
+            }
+        }
+        public int Distance{
+            get{
+                return (int) (100 * _fuelLevel / FuelConsumption);
+            }
+            set{
+                
+            }
+        }
         private double _mileage;
         private double _fuelLevel;
-        public readonly double FuelTankCapacity;
-        public readonly double FuelConsumption;
+        public double FuelTankCapacity {get; init;}
+        public double FuelConsumption {get; init;}
         public void Refuel(){
             _fuelLevel = FuelTankCapacity;
         }
-        public double GetMileage(){
-            return _mileage;
+        public double GetMileage{
+            get{
+                return _mileage;
+            }
+            set{
+                if ( _mileage > 0){
+                    _mileage = value;
+                }
+            }
         }
-        public double GetFuelLevel()
-        {
-            return _fuelLevel;
+        public double GetFuelLevel{
+            get{
+                return _fuelLevel;
+            }
+            set{
+                if (_fuelLevel > 0){
+                    _fuelLevel= value;
+                }
+            }
         }
         public Car(int power, int volume, double fuelTankCapasity, double fuelConsumption){
             Power = power;
@@ -49,7 +96,6 @@
             //Dodaj logike sprawdzającą czy mamy tyle paliwa i obliczenie ile rzeczywiście przejechał samochód
             _fuelLevel -= fuel;
             _mileage += distance;
-
         }
     }
 }
