53e10e1e20e0ffacb76539ebfb45b25d63cc8801
Wojciech Pietrzak <118462265+Usadowiony@users.noreply.github.com>
2023-11-29 09:41:19 +0100

    add Laboratorium8

---
+++ Laboratorium8/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        StringCreation();
+        StringIteration();
+    }
+
+    public static void StringCreation()
+    {
+        string Message = "abcd";
+        string MessageInterpolated = $"Value of Message '{Message}'";
+        string MessageRaw =
+@"line 1
+line2";
+        char[] Chars = { 'a', 'b', 'c', 'd' };
+        string MessageFromChars = new string(Chars);
+    }
+
+    public static void StringIteration()
+    {
+        string msg = "abcd";
+        for(int i =0; i < msg.Length; i++)
+        {
+            char c = msg[i];
+            int d = msg[i];
+        }
+        foreach(char c in msg)
+        {
+            Console.WriteLine(c);
+        }
+        foreach (int d in msg)
+        {
+            Console.WriteLine(d);
+        }
+    }
+}
