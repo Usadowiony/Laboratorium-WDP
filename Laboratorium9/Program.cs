677e96a8f94e7c1077561efd429ad5fb45ab1875
Wojciech Pietrzak <118462265+Usadowiony@users.noreply.github.com>
2023-12-06 09:52:38 +0100

    commit

--- Laboratorium9/Program.cs
+++ Laboratorium9/Program.cs
@@ -10,10 +10,19 @@
                 StudyField = "Informatyka stosowana"};
             Console.WriteLine(student);
             student.ToString();
+            Student s = new Student("Ewa", DateTime.Parse("2000-10-10"), "Ekonomia");
+            object studentObject = student;
+            Person studentAsPerson = student;
         }
         public class Person {
             public string Name { get; set; }
             public DateTime Birth { get; set; }
+
+            public Person(string name, DateTime birth) {
+                Name = Name;
+                Birth = Birth;
+            }
+            public Person() { }
 
             public int GetAge(DateTime now)
             {
