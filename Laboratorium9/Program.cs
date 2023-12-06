8a721aa020096812af6d556c60c3fc8dd77d3087
Wojciech Pietrzak <118462265+Usadowiony@users.noreply.github.com>
2023-12-06 09:26:58 +0100

    add Laboratorium9

---
+++ Laboratorium9/Program.cs
@@ -0,0 +1,21 @@
+﻿ namespace Laboratorium9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public class Person {
+            public string Name { get; set; }
+            public DateTime Birth { get; set; }
+
+            public int GetAge(DateTime now)
+            {
+                return now.Year - Birth.Year;
+            }
+        }
+        public class Student:Person{ 
+        }
+    }
+}
