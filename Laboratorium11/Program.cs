d390ccf9e1ed20e7ca43211322213fdd5f47a89c
Wojciech Pietrzak <118462265+Usadowiony@users.noreply.github.com>
2023-12-20 09:59:03 +0100

    change laboratorium11

--- Laboratorium11/Program.cs
+++ Laboratorium11/Program.cs
@@ -3,28 +3,73 @@
     static void Main(string[] args)
     {
         IEnumerable<int> evens = Evens();
-        foreach (int number in evens) { 
+        foreach (int number in evens)
+        {
             Console.WriteLine(number * number);
         }
+
         for (IEnumerator<int> enumerator = evens.GetEnumerator(); enumerator.MoveNext();)
         {
             int number = enumerator.Current;
             Console.WriteLine(number);
         }
+
+        foreach (int number in EvensGenerator(1, 7))
+        {
+            Console.WriteLine(number * number);
+        }
+        foreach (string name in NamesGenerator())
+        {
+            Console.WriteLine(name);
+        }
+
+        ICollection<string> names = NamesCollection();
+        if (names.Contains("Beata"))
+        {
+            Console.WriteLine("Jest");
+        }
+        else
+        {
+            Console.WriteLine("Brak");
+        }
+        names.Add("Robert");
+        names.Remove("Adam");
+        Console.WriteLine("==========");
+        Console.WriteLine(names.Count);
+        foreach (string name in names)
+        {
+            Console.WriteLine(name.ToUpper());
+        }
     }
+
+    static ICollection<string> NamesCollection()
+    {
+        List<string> names = new List<string>(){
+            "Adam",
+            "Ewa",
+            "Karol",
+            "Beata"
+        };
+        return names;
+    }
+
     public static IEnumerable<int> Evens()
     {
         int[] result = { 2, 4, 6, 8 };
         return result;
     }
-    
-    public static IEnumerable<string> Names()
+
+    public static IEnumerable<string> NamesGenerator()
     {
-        string[] result = { "Adam", "Ewa", "Karol" };
-        return result;
+        yield return "Adam";
+        yield return "Ewa";
+        yield return "Karol";
     }
     public static IEnumerable<int> EvensGenerator(int start, int end)
     {
-        for (int i = start; i < end; i++) ;
+        for (int i = start + start % 2; i <= end; i += 2)
+        {
+            yield return i;
+        }
     }
 }
